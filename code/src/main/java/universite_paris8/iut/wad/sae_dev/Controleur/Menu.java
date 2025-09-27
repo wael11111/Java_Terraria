225483ab53044fc281a60f4659779161811c5076
dje <diallodjenebaa93@icloud.com>
2025-09-27 19:31:26 +0200

    Refactoring de la classe Menu

--- code/src/main/java/universite_paris8/iut/wad/sae_dev/Controleur/Menu.java
+++ code/src/main/java/universite_paris8/iut/wad/sae_dev/Controleur/Menu.java
@@ -20,29 +20,14 @@ public class Menu {
         int largeurFenetre = terrain.getLargeurPixels();
         int hauteurFenetre = terrain.getHauteurPixels();
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/universite_paris8/iut/wad/sae_dev/fxml/jeu.fxml"));
-        Scene scene = new Scene(loader.load(), largeurFenetre, hauteurFenetre);
-
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setScene(scene);
-        stage.setTitle("Sugar Rush");
-        stage.setResizable(false);
-        stage.show();    }
+        changerScene(event, "/universite_paris8/iut/wad/sae_dev/fxml/jeu.fxml", largeurFenetre, hauteurFenetre);
+    }
 
     @FXML
     private void afficherRegles(ActionEvent event) throws IOException {
         System.out.println("Afficher les règles");
-
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/universite_paris8/iut/wad/sae_dev/fxml/regles.fxml"));
-        Scene scene = new Scene(loader.load(), 900, 600);
-
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setScene(scene);
-        stage.setTitle("Sugar Rush");
-        stage.setResizable(false);
-        stage.show();
+        changerScene(event, "/universite_paris8/iut/wad/sae_dev/fxml/regles.fxml", 900, 600);
     }
-
 
     @FXML
     private void quitterJeu(ActionEvent event) {
@@ -53,10 +38,12 @@ public class Menu {
     @FXML
     private void retournerMenu(ActionEvent event) throws IOException {
         System.out.println("Retourner au menu");
+        changerScene(event, "/universite_paris8/iut/wad/sae_dev/fxml/menu.fxml", 900, 600);
+    }
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/universite_paris8/iut/wad/sae_dev/fxml/menu.fxml"));
-        Scene scene = new Scene(loader.load(), 900, 600);
-
+    private void changerScene(ActionEvent event, String cheminFxml, int largeur, int hauteur) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(cheminFxml));
+        Scene scene = new Scene(loader.load(), largeur, hauteur);
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setScene(scene);
         stage.setTitle("Sugar Rush");
